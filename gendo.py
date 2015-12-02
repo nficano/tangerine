9c9895019ef05899780e783e5359a74d81748cc4
Nick Ficano <nficano@gmail.com>
2015-12-02 18:49:20 -0500

    no more image me

--- gendo.py
+++ gendo.py
@@ -1,9 +1,5 @@
 import datetime
 import os
-import random
-import re
-
-import requests
 from gendo import Gendo
 
 path = os.path.dirname(os.path.abspath(__file__))
@@ -23,51 +19,6 @@ def morning(user, message):
         now = datetime.datetime.now()
         if now.time() < datetime.time(12):
             return "mornin' @{user.username}"
-
-
-@gendo.listen_for('image me')
-def image_me(user, message):
-    matches = re.findall('image me(.*)', message)
-    if not matches:
-        return
-    query = matches[0].strip()
-    results = _search_google_images(query)
-    if results:
-        while True:
-            image_url = _get_random_image(results)
-            if _verify_image(image_url):
-                return image_url
-
-
-def _verify_image(url):
-    resp = requests.get(url)
-    is_working = resp.ok
-    content_type = resp.headers.get('Content-Type', '')
-    if is_working and 'image' in content_type:
-        return True
-    return False
-
-
-def _search_google_images(query):
-    results = []
-    url = 'https://ajax.googleapis.com/ajax/services/search/images'
-    params = {
-        'v': '1.0',
-        'q': query,
-        'safe': gendo.settings.get('image_me', {}).get('safe_search', 'off')
-    }
-    if 'gif' in query:
-        params['as_filetype'] = 'gif'
-    resp = requests.get(url, params=params)
-    if resp.ok:
-        results = resp.json().get('responseData', {}).get('results', [])
-    return results
-
-
-def _get_random_image(results):
-    min_int, max_int = (0, len(results) - 1)
-    idx = random.randint(min_int, max_int)
-    return results[idx].get('url')
 
 if __name__ == '__main__':
     gendo.run()
