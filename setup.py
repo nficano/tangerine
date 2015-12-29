4856c5827f9e0190592b4afd846057d81f1a045c
Nick Ficano <nficano@gmail.com>
2015-12-28 22:46:51 -0500

    updated python versions

--- setup.py
+++ setup.py
@@ -34,7 +34,12 @@ setup(
     long_description=readme,
     license=license,
     classifiers=[
+        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python",
         "Topic :: Internet",
     ]
 )
