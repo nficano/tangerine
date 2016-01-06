61df62b68ddb77b82745b04575023a3bc785740a
Ben Lewis <ben@keep.com>
2016-01-06 20:56:18 +0000

    Adding support for callable rules (worth it?)

--- gendo/bot.py
+++ gendo/bot.py
@@ -3,6 +3,7 @@
 from __future__ import absolute_import
 import json
 import logging
+import inspect
 import datetime
 import os
 import sys
@@ -11,6 +12,7 @@ import time
 from slackclient import SlackClient
 from .scheduler import Task
 from . import __version__
+import six
 import yaml
 
 log = logging.getLogger(__name__)
@@ -31,7 +33,38 @@ class Gendo(object):
             settings = yaml.load(ymlfile)
             return cls(settings=settings)
 
+    def _verify_rule(self, supplied_rule):
+        """Rules must be callable with (user, message) in the signature.
+        Strings are automatically converted to callables that match.
+
+        :returns: Callable rule function with user, message as signature.
+        :raises ValueError: If `supplied_rule` is neither a string nor a
+                            callable with the appropriate signature.
+        """
+        # If basestring, make a simple match callable
+        if isinstance(supplied_rule, basestring):
+            return lambda user, message: supplied_rule in message.lower()
+
+        if not six.callable(supplied_rule):
+            raise ValueError('Bot rules must be callable or strings')
+
+        expected = ('user', 'message')
+        signature = tuple(inspect.getargspec(supplied_rule).args)
+        try:
+            # Support class- and instance-methods where first arg is
+            # something like `self` or `cls`.
+            assert len(signature) in (2, 3)
+            assert expected == signature or expected == signature[-2:]
+        except AssertionError:
+            msg = 'Rule signuture must have only 2 arguments: user, message'
+            raise ValueError(msg)
+
+        return supplied_rule
+
     def listen_for(self, rule, **options):
+        """Decorator for adding a Rule. See guidelines for rules.
+        """
+        rule = self._verify_rule(rule)
         def decorator(f):
             self.add_listener(rule, f, **options)
             return f
@@ -76,8 +109,8 @@ class Gendo(object):
         elif message == 'gendo version':
             self.speak("Gendo v{0}".format(__version__), channel)
             return
-        for phrase, view_func, options in self.listeners:
-            if phrase in message.lower():
+        for rule, view_func, options in self.listeners:
+            if rule(user, message):
                 response = view_func(user, message, **options)
                 if response:
                     if '{user.username}' in response:
