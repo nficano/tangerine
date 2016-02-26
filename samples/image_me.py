94e96ae99e37b348a3eaf20a83b7e79a4c213e74
Nick Ficano <nficano@gmail.com>
2016-02-25 22:38:52 -0500

    added examples of bots for funsies

---
+++ samples/image_me.py
@@ -0,0 +1,45 @@
+"""
+    google image me
+    ~~~~~~~~~~~~~~~
+
+    image me -- ask me for an image of anything
+
+    # Sample usage:
+    from gendo import Gendo
+    import image_me as google
+
+    gendo = Gendo('xoxb-your-key-here')
+
+    @gendo.listen_for('image me')
+    def image_me(user, message):
+        query = message.replace('image me', '')
+        return google.random_google_image_search(query)
+
+    if __name__ == '__main__':
+        gendo.run()
+"""
+import requests
+import random
+
+URL = "https://www.googleapis.com/customsearch/v1"
+CSE_KEY = ""
+CSE_ID = ""
+
+
+def google_image_search(query):
+    resp = requests.get(URL, params={
+        'v': '1.0', 'searchType': 'image', 'q': query.strip(),
+        'cx': CSE_ID, 'key': CSE_KEY})
+    if not resp.ok:
+        yield
+    for i in resp.json().get('items', []):
+        yield i.get('link')
+
+
+def random_google_image_search(query):
+    if query:
+        return random.choice([r for r in google_image_search(query)])
+
+
+if __name__ == "__main__":
+    print random_google_image_search("nick ficano")
---
+++ samples/reddit_rando.py
@@ -0,0 +1,74 @@
+"""
+    reddit rando (comment)
+    ~~~~~~~~~~~~~~~~~~~~~~
+
+    Get a random comment from a given subreddit.
+
+    # Sample usage:
+
+    import reddit_rando as reddit
+    from gendo import Gendo
+
+    gendo = Gendo('xoxb-your-key-here')
+
+    @gendo.listen_for('rando comment')
+    def rando(user, message):
+        subreddit = message.replace('rando comment', '').strip()
+        ok, comment = reddit.get_random_comment_in_subreddit(subreddit)
+        if not ok:
+            return 'oh hamburgers. {0}'.format(comment)
+        return comment
+
+    if __name__ == '__main__':
+        gendo.run()
+"""
+import requests
+import random
+
+
+def get_posts_in_subreddit(subreddit):
+    url = 'https://www.reddit.com/r/{0}/.json'.format(subreddit)
+    ok, res = reddit_request(url)
+    if not ok:
+        yield
+    for child in res.get('data', {}).get('children', []):
+        permalink = child.get('data', {}).get('permalink')
+        url = "https://www.reddit.com{0}.json".format(permalink)
+        yield url
+
+
+def get_comments_in_post(url):
+    ok, res = reddit_request(url)
+    if not ok:
+        yield
+    for comment in res[1].get('data', {}).get('children', []):
+        body = comment.get('data', {}).get('body', '').strip()
+        if len(body) < 800 and 'http' not in body:
+            yield body
+
+
+def get_random_comment_in_subreddit(subreddit):
+    urls = [url for url in get_posts_in_subreddit(subreddit)]
+    if not urls:
+        return False, "no posts."
+    url = random.choice(urls)
+    comments = [c for c in get_comments_in_post(url)]
+    if not comments:
+        return get_random_comment_in_subreddit(subreddit)
+    return True, random.choice(comments)
+
+
+def reddit_request(url):
+    resp = requests.get(url, headers={
+        'User-agent': 'darwin:slackbot:v1.2.3 (by /u/nficano)'
+    })
+    if not resp.ok:
+        return False, resp
+    return True, resp.json()
+
+
+if __name__ == '__main__':
+    subreddit = 'mildlyinteresting'
+    ok, comment = get_random_comment_in_subreddit(subreddit)
+    if ok:
+        print comment
