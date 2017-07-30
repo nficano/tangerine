4ab90578a6311095dbf6f492c5e7020b7251565d
Nick Ficano <nficano@gmail.com>
2017-07-30 14:51:54 -0400

    Bump version: 3.0.3 → 4.0.0

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     name='gendobot',
     author='Nick Ficano',
     author_email='nficano@gmail.com',
-    version='3.0.3',
+    version='4.0.0',
     packages=find_packages(exclude=['tests*']),
     url='http://nickficano.com',
     description='a lightweight Slackbot framework for Python',
