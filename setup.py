bf871fecb4eaee6d4d714df07477590571981788
Nick Ficano <nficano@gmail.com>
2018-10-19 12:47:46 -0400

    Bump version: 5.0.1 → 5.0.2

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ class UploadCommand(Command):
 
 setup(
     name='slack-tangerine',
-    version='5.0.1',
+    version='5.0.2',
     author='Nick Ficano',
     author_email='nficano@gmail.com',
     packages=['tangerine'],
